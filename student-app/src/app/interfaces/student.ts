a0a78b5f5925fd7bfca76a1771c877e7a4eeecb7
Skerdi Shehaj <skerdianshehaj@gmail.com>
2024-04-10 23:30:12 +0200

    Lab 1-2: First commit: student-app

---
+++ student-app/src/app/interfaces/student.ts
@@ -0,0 +1,6 @@
+export interface Student {
+  id: number;
+  username: string;
+  name: string;
+  email: string;
+}
---
+++ student-app/src/app/students/students.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {Student} from "../interfaces/student";
+import {StudentService} from "../services/student.service";
+
+@Component({
+  selector: 'app-students',
+  templateUrl: './students.component.html',
+  styleUrls: ['./students.component.css']
+})
+export class StudentsComponent implements OnInit {
+
+  students!: Student[];
+
+  constructor(private studentService: StudentService) { }
+
+  ngOnInit(): void {
+    this.getStudents();
+  }
+
+  getStudents(): void {
+    this.studentService.getStudents()
+      .subscribe(students => this.students = students);
+  }
+
+  delete(student: Student): void {
+    console.log(`Studenti ${student.id}: ${student.name} u fshi`);
+  }
+
+}
